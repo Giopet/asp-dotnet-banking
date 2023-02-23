29c54dd76d1b36d988c075c8bda1981819979cb2
Giopet <petasgiorgos@gmail.com>
2023-02-24 00:12:12 +0200

    create accounts controller

---
+++ Banking.API/Controllers/AccountsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banking.API.Controllers
+{
+    /// <summary>
+    /// Created by right click on Controllers -> Add -> API (Empty)
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetAccounts()
+        {
+            return "OK";
+        }
+    }
+}
