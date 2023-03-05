6b2a2f4e04d18845e8136090e6ed4107d29a9237
Giopet <petasgiorgos@gmail.com>
2023-03-05 20:52:11 +0200

    retrieving data & added http statuses

--- Banking.API/Controllers/AccountsController.cs
+++ Banking.API/Controllers/AccountsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Banking.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Banking.API.Controllers
 {
@@ -12,10 +14,54 @@ namespace Banking.API.Controllers
     [ApiController]
     public class AccountsController : ControllerBase
     {
-        [HttpGet]
-        public string GetAccounts()
+        private readonly BankingContext _context;
+
+        public AccountsController(BankingContext context)
         {
-            return "OK";
+            _context = context;
+
+            // Runs the seed if that hasn't taken place
+            _context.Database.EnsureCreated(); 
+        }
+
+        ///// <summary>
+        ///// Use without ActionResult when you assume you will have all the time http 200 and no errors.
+        ///// </summary>
+        ///// <returns></returns>
+        //[HttpGet]
+        //public IEnumerable<Account> GetAllAccounts()
+        //{
+        //    return _context.Accounts.ToList();
+        //}
+
+        /// <summary>
+        /// Return HTTP status codes with a payload of http response as the accounts.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult> GetAllAccounts()
+        {
+            return Ok(await _context.Accounts.ToListAsync());
+        }
+
+        /// <summary>
+        /// We can use just the id on Route because we have the base Route attribute on class level.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")] // Abbreviation
+        //[HttpGet][Route("{id}")]
+        //[HttpGet][Route("/api/accounts/{id}")]
+        public async Task<ActionResult> GetAccount(int id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+
+            if(account == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(account);
         }
     }
 }
